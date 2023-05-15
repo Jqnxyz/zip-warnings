a44d1f6f1a830770447782468be0c2d3a8b921bd
Jerome Quah <jerome.quah@wego.com>
2023-05-15 15:26:23 +0800

    Update favicon

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,8 @@ export default function Nextra({ Component, pageProps }) {
           type="font/woff2"
           crossOrigin="anonymous"
         />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
       </Head>
       <Component {...pageProps} />
     </>
